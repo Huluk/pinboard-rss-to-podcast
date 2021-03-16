203d59a28ddf502f145213bc0bf2b81441ddb467
Lars Hansen <LarsHuluk@gmail.com>
2021-03-16 17:23:18 +0100

    Add itunes and atom prefixes

--- web.rb
+++ web.rb
@@ -10,7 +10,7 @@ LENGTH_REQUEST_TIMEOUT = 1
 
 Header = <<-END
 <?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
+<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
   <channel>
     <title>%{title}</title>
     <description>%{description}</description>
