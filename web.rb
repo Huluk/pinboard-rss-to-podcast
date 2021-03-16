1a40fbcf08cf8b4c43760c1724e08e0093a42b49
Lars Hansen <LarsHuluk@gmail.com>
2021-03-16 16:25:25 +0100

    Add content-type and url escaping

--- web.rb
+++ web.rb
@@ -6,7 +6,7 @@ require 'net/http'
 
 Pinboard = "https://feeds.pinboard.in/rss/"
 
-Header = <<END
+Header = <<-END
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
   <channel>
@@ -17,7 +17,7 @@ Header = <<END
 
 END
 
-Item = <<END
+Item = <<-END
     <item>
       <title>%{title}</title>
       %{optional_author}
@@ -28,13 +28,17 @@ Item = <<END
 
 END
 
-Footer = <<END
+Footer = <<-END
   </channel>
 </rss>
 END
 
 def esc(str)
   CGI.escapeHTML str
+end
+
+def url_esc(str)
+  str.to_s.gsub(/&/, "&amp;")
 end
 
 def parse_children(node)
@@ -57,7 +61,8 @@ def item_data(item)
   end
   attributes[:title] = esc attributes[:title]
   attributes[:description] = esc attributes[:description]
-  uri = URI(attributes[:link])
+  attributes[:link] = url_esc attributes[:link]
+  # uri = URI(attributes[:link])
   # attributes[:length] =
   #   Net::HTTP.new(uri.host).request_head(uri.path)['Content-Length']
   return attributes
@@ -73,5 +78,6 @@ get '/*' do
     item = rss.at_xpath(%Q|//xmlns:item[@rdf:about="#{audio_path}"]|)
     Item % item_data(item)
   }
+  headers "Content-Type" => "text/xml"
   header + elements.join("\n") + Footer
 end
